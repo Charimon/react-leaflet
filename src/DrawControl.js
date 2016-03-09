1b229002da141fa5753565990f1c9f29e382226f
Charimon <acharkin@gmail.com>
2016-03-09 02:46:29 -0800

    fixed draw control example

--- src/DrawControl.js
+++ src/DrawControl.js
@@ -1,19 +1,20 @@
 import { PropTypes } from 'react';
 import { control } from 'leaflet';
-import test from 'leaflet-draw';
+import DR from 'leaflet-draw';
 
 import MapControl from './MapControl';
 
 export default class DrawControl extends MapControl {
-  static propTypes = {
-    polygon: PropTypes.string,
-    polyline: PropTypes.string,
-    circle: PropTypes.string,
-    rectangle: PropTypes.string,
-    marker: PropTypes.string
-  };
+  // static propTypes = {
+  //   polygon: PropTypes.string,
+  //   polyline: PropTypes.string,
+  //   circle: PropTypes.string,
+  //   rectangle: PropTypes.string,
+  //   marker: PropTypes.string
+  // };
 
   componentWillMount() {
-    this.leafletElement = control.zoom(this.props);
+    this.leafletElement = new L.Control.Draw(this.props);
+    // this.leafletElement = control.zoom(this.props);
   }
 }
